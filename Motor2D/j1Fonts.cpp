29a71ecd0206b0c38b4df56c8883385322e02627
HeladodePistacho <th_sola@hotmail.com>
2017-04-21 19:32:41 +0200

    Changed Print method to add new lines

--- Motor2D/j1Fonts.cpp
+++ Motor2D/j1Fonts.cpp
@@ -74,10 +74,10 @@ TTF_Font* const j1Fonts::Load(const char* path, int size)
 }
 
 // Print text using font
-SDL_Texture* j1Fonts::Print(const char* text, SDL_Color color, TTF_Font* font)
+SDL_Texture* j1Fonts::Print(const char* text, int new_line, SDL_Color color, TTF_Font* font)
 {
 	SDL_Texture* ret = NULL;
-	SDL_Surface* surface = TTF_RenderText_Blended((font) ? font : default, text, color);
+	SDL_Surface* surface = TTF_RenderText_Blended_Wrapped((font) ? font : default, text, color, new_line);
 
 	if(surface == NULL)
 	{
