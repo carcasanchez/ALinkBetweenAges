2f64222a8e8330619a0a51a6d8fc66f34a3961e4
Fran <fran152job@gmail.com>
2017-05-13 18:12:08 +0200

    Bombs outside castle finished

--- Motor2D/j1GameLayer.cpp
+++ Motor2D/j1GameLayer.cpp
@@ -92,7 +92,7 @@ bool j1GameLayer::Update(float dt)
 
 	if (App->input->GetMouseButtonDown(SDL_BUTTON_LEFT) == KEY_DOWN)
 		//em->CreateNPC(1, NPC_DARK_ZELDA, mousePos.x, mousePos.y);
-		App->sceneM->RequestSceneChange({0,0}, "bossRoom", D_DOWN);
+		App->sceneM->RequestSceneChange({0,0}, "outsideCastle", D_DOWN);
 		//em->CreateObject(1, mousePos.x, mousePos.y, LIFEHEART);
 	 if (App->input->GetMouseButtonDown(SDL_BUTTON_RIGHT) == KEY_DOWN)
 	{
@@ -238,6 +238,8 @@ void j1GameLayer::PickObject(Object * object)
 		if (em->player->staminaPotions < em->player->maxStaminaPotions)
 			em->player->staminaPotions++;
 
+	case BOMB_SAC:
+		em->player->bombs += 5;
 	default:
 		em->player->inventory.push_back(object->objectType);
 		App->audio->PlayFx(23);
