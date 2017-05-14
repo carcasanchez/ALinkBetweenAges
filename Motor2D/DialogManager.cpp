e1a93ea1b87d1c5e54ef4efdda7cd4d8af1a1443
HeladodePistacho <th_sola@hotmail.com>
2017-05-14 20:21:01 +0200

    Moved text position for full screen mode

--- Motor2D/DialogManager.cpp
+++ Motor2D/DialogManager.cpp
@@ -63,7 +63,7 @@ bool DialogManager::Start()
 	screen = App->gui->CreateScreen(screen);
 	text_on_screen = (UI_String*)App->gui->Add_element(STRING, this);
 	text_on_screen->Set_Active_state(false);
-	text_on_screen->Set_Interactive_Box({ 150, 600, 390, 20 });
+	text_on_screen->Set_Interactive_Box({ 270, 750, 390, 20 });
 	text_on_screen->SetBlitTimeMS(20);
 
 	screen->AddChild(text_on_screen);
